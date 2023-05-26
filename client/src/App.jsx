f94bd461410cb2a979f4b0addaeeaae75260fc10
Lokteeva <swsw06@mail.ru>
2023-05-26 09:50:14 +0300

    perenos is india2024

---
+++ client/src/App.jsx
@@ -0,0 +1,181 @@
+import './App.css';
+import React from 'react';
+import myVideo from './video/logo2.mp4';
+
+
+function App() {
+        return(
+        <div className="app">
+
+ <div className='banner2'>
+    <img src="/banner.jpg" width="100%" alt="img"/>
+    <h5 className='banner1 h5'>      
+      Паломничество в Индию с <span className='textOrange'> Бхата Бхгатамрита Кешава Свами</span><br/>
+                      Февраль - Март 2024<br/>
+                      2 недели паломнического путешествия в <span className='textOrange'>Чарупати, Бомбей , Нешик , Джуху</span>
+      
+    </h5>
+    <h5 className='banner3 h5'>Встреча с удивительным<br/>
+                            Кришной и Его преданными<br/>
+                            с   погружением  в духовную атмосферу<br/>
+                            в обществе преданных</h5>
+</div>
+
+<div className="btn" >     
+<div></div>                 
+  <button className="btn2" >Контакты</button>
+  <div></div>
+  <button className="btn2" >Заявка</button>
+  <div></div>
+</div> 
+
+<center>               
+<h2><span>НЫРНУТЬ</span> в океан блаженства и <span>ПРОБУДИТЬ</span> духовную силу в обществе преданных</h2>
+</center> 
+
+<div className='liSection'>
+<div >
+        <video autoPlay muted loop id="video" width="100%">
+        <source src={myVideo} type="video/mp4" />
+        </video>
+ </div> 
+  <div>
+  <img src="/swami.jpg" alt="img"/>
+  </div>
+</div>
+       
+       <div className="lisection">
+        <h2 className=''>Вам <span >НУЖНО</span> особенное время :</h2>
+         <div className='center-div'>
+         <ul>
+            <li>Погрузится в духовный мир</li>
+            <li>Наполниться силой</li>
+            <li>Получить даршан Гуру</li>
+            <li>Отдохнуть  в атмосфере благости</li>
+            <li>Провести время с семьей</li>
+          </ul>
+         </div>
+         <div className='grid3col'>
+          <img className='imgS' src="pic1.jpg" alt="img"/>
+          <img className='imgS' src="pic2.jpg" alt="img"/>
+          <img className='imgS' src="pic3.jpg" alt="img"/>
+        </div>
+ <div>
+    <h2 >Паломничество с <span>ГУРУ</span> что будет :</h2>
+    <div className='center-div'>
+          <ul>
+            <li>2 недели погружения в атмостферу Индии</li>
+            <li>Душевные киртаны с духовным учителем и преданными</li>
+            <li>Джапа медитация с эффективными настройками</li>
+          </ul> 
+    </div>  
+</div>
+
+
+<div>
+<div className='grid3col'>
+          <img className='imgS' src="pic4.jpg" alt="img"/>
+          <img className='imgS' src="pic5.jpg" alt="img"/>
+          <img className='imgS' src="pic6.jpg" alt="img"/>
+        </div>
+
+</div>
+</div>
+
+<h2><span>КАК</span> паломничество <span>ПОМОГАЕТ</span> в духовной жизни?</h2>
+
+<div className='grid2col'>
+<img src="https://hiasia.xyz/Uploads/Picture/NhuTran-184519024520-Elephanta-Island.jpg" className="border2storoni" alt="img"></img>
+<div>
+<ul>
+  <li>Атмосфера паломничества — уникальное поле доверия, в котором происходит соприкосновение и раскрытие сердец, <strong>глубокое переживание опыта</strong> </li>
+   <li>Вы получите не просто новую информацию, а сделаете определенные шаги на практике.
+<strong> Ваша джапа и садхана улучшатся</strong>, а служение станет более осознанным и качественным</li>
+<li>Во время паломничества Вы <strong>очистите свое сердце</strong> от претензий и обид и наполните его духовной энергией, научитесь эффективно сотрудничать в отношениях с преданными, глубоко проживете полученные знания, которые станут вашим духовным опытом</li>
+</ul>
+</div>
+</div>
+
+
+
+<h2 className='textOrange'><>ЧТО</> Вас <>ОЖИДАЕТ</></h2>
+<div className='grid2col'>
+  <div>
+  <h2>Мы посетим такие <span>ГОРОДА</span> как:</h2>
+<div className='otstup'>
+<details>
+   <summary>Чаупати</summary>
+   <p>Или Чоупатти (англ. Chaupati) — известный пляж в Мумбаи. Расположен в городском округе Гиргаум в непосредственной близости от Марин-драйв. На Чаупати с особой пышностью ежегодно проводится индуистский фестиваль Ганеша-чатуртхи, в ходе которого верующие омывают божества Ганеши в Аравийском море. В переводе чаупати означает «четыре ручья» или «четыре канала».
+   </p>
+</details>
+
+<details>
+   <summary>Бомбей</summary>
+   <p>Или Мумбаи (англ. Mumbai), до 1995 года — Бомбе́й (англ. Bombay) — город на западе Индии, на побережье Аравийского моря. Административный центр штата Махараштра. Население — 12 422 373 человека по состоянию на 2011 год. Является наибольшим в стране по численности жителей.
+   </p>
+</details>
+
+<details>
+   <summary>Нешик</summary>
+   <p>информация не доступна</p>
+</details>
+
+<details>
+   <summary>Джуху</summary>
+   <p> (произношение: [dʒuɦuː]) - пригород Мумбаи. Он известен своим обширным пляжем Джуху. Он окружен Аравийским морем на западе, Версовой на севере, Виле-Парле на востоке и Сантакрузом на юге. Джуху является одним из самых дорогих и богатых районов столичного региона и домом для многих знаменитостей Болливуда. 
+   </p>
+</details>
+</div>
+  </div>
+  <div>
+  <h2><span>ЗАЖИГАТЕЛЬНЫЕ</span> киртаны</h2>
+<img className="border2storoni" src="https://i.ytimg.com/vi/nDDfQ_dOsb0/maxresdefault.jpg" alt="img"/>
+  </div>
+</div>
+
+
+<div className='grid2col'>
+<div className="liSection2">
+<h2><span>БХАДЖАНЫ</span> с душой</h2>
+<div >
+<img className="border2storoni" src="http://gauragorsk.ru/images/NASTA/miscellaneous/bbks5.jpg"  alt="img"></img>
+</div>   
+</div> 
+
+<div className="1liSection">
+        <h2 >Особенно <span>ЯРКИЕ</span> впечатления </h2>
+        
+ <div >
+ <ul>
+    <li>Святые места</li>
+    <li>Эффективные практики</li>
+    <li>Бесценные знания</li>
+  </ul>
+ </div>
+</div>
+</div>
+
+<h3 >МЫ БУДЕМ <span>ОЧЕНЬ</span> ВАМ РАДЫ!!!</h3>
+<div className='grid3col2'>
+  <div></div>
+<img className="border2storoni" src="https://i.ytimg.com/vi/4w85Ya_KGhk/maxresdefault.jpg"  alt="img"></img>
+<div></div>
+</div>
+
+<h3 >ВАМ ЭТО <span>НУЖНО</span>!!! </h3>               
+
+
+
+
+<footer >
+<div>
+<p>По всем вопросам пишите/звоните: </p>
+ <p>+7(777)777-77-77</p>
+</div>
+<p>Спасибо что Вы с нами!</p>
+ </footer>
+
+</div>)
+        
+}
+export default App
